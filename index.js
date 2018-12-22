25a832c6840b3148227a554311acc148efda886e
Felice Forby <feliceforby@gmail.com>
2018-12-22 00:30:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Note: var should never be used to declare variables, because of it's weird
+// scoping issues (it is NOT block scoped)
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  // NOT a good idea to declare a variable like this, because
+  // it ends up in global scope
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  // See the consequences of declaring a variable in global scope
+  bestCustomer = 'maybe bob'; // we would want this to stay in the function scope!
+}
+
+const leastFavoriteCustomer = 'jim';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bob";
+}
