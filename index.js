0affb72c70449e74f24197435baa376d1a2328c3
Felice Forby <feliceforby@gmail.com>
2018-12-22 00:09:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Note: var should never be used to declare variables, because of it's weird
 // scoping issues (it is NOT block scoped)
-var customerName;
+var customerName = 'bob';
 
 function upperCaseCustomerName(name) {
   return name.toUpperCase();
